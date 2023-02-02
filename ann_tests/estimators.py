4cf2738bc182737b57b9d197d60e449b91a5e2f3
Matthew S. Baran <matthewsbaran@gmail.com>
2023-02-01 20:39:54 -0500

    Move dependencies to environment.yml.  Add github actions CI build.  Format code and update readme.

--- ann_tests/estimators.py
+++ ann_tests/estimators.py
@@ -3,7 +3,6 @@ from sklearn.neural_network import MLPRegressor
 
 
 def get():
-
     estimator_list = [
         MLPRegressor(hidden_layer_sizes=(5, 5), learning_rate_init=0.01, early_stopping=True),
         MLPRegressor(hidden_layer_sizes=(50, 50), learning_rate_init=0.01, early_stopping=True),
